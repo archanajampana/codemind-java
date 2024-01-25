12c2a2db10096682a94f88aaf8ea0838296fe040
archanajampana <119840726+archanajampana@users.noreply.github.com>
2024-01-25 09:44:09 +0530

    import java.util.Scanner; public class Tickets{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if((4*a)<=1000){             System.out.println("YES");         }         else{             System.out.println("NO");         }
        }
    }

---
+++ Tickets.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Tickets{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if((4*a)<=1000){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+        
+    }
+}
