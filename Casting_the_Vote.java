c27692e3f61d01923cef862d84aa5acba2d6600f
archanajampana <119840726+archanajampana@users.noreply.github.com>
2024-01-25 12:07:16 +0530

    import java.util.Scanner; public class CastingVote {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a= sc.nextInt();         if(a>=18)         System.out.println("YES");         else         System.out.println("NO");
        }
    }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CastingVote
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        if(a>=18)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+        
+        
+    }
+}
